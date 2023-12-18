7dcefe279e29e56b41d113f308c7a7ddd7f20bc1
tamarheisler <113795766+tamarheisler@users.noreply.github.com>
2023-12-18 12:27:51 +0200

    Update main.js

--- static/main.js
+++ static/main.js
@@ -2,37 +2,23 @@ function download() {
     var url = document.getElementById('url').value;
     var choice = document.getElementById('choice').value;
     var format = document.getElementById('format').value;
-
-    // Additional validation logic if needed
-
-    // Example: Check if the URL is not empty
     if (!url.trim()) {
         document.getElementById('errorMessage').innerHTML = 'Please enter a valid URL.';
         return;
     }
-
-    // Check if the URL is valid
     var urlRegex = /^(https?:\/\/)?(www\.)?youtube\.com\/(watch\?v=|playlist\?list=|channel\/)[\w-]+$/;
     if (!urlRegex.test(url)) {
         document.getElementById('errorMessage').innerHTML = 'Please enter a valid YouTube URL.';
         return;
     }
-
-    // Reset error and success messages
     document.getElementById('errorMessage').innerHTML = '';
     document.getElementById('successMessage').innerHTML = '';
-
     document.getElementById('loadingSpinner').style.display = 'block';
-
     var elementsToHide = document.getElementsByClassName("formElement");
     for (var i = 0; i < elementsToHide.length; i++){
         elementsToHide[i].style.display = "none";
     }
-
-    // Generate a random parameter to avoid caching
     var randomParam = Math.random().toString(36).substring(7);
-
-    // Fetch API request to the Flask endpoint with the random parameter
     fetch('/download?random=' + randomParam, {
         method: 'POST',
         headers: {
@@ -51,8 +37,6 @@ function download() {
             document.getElementById('errorMessage').innerHTML = "Error while downloading file";
             document.getElementById('errorMessage')   .style.display='block'
         }
-
-        // Call the additional function after downloading the video
         afterDownload();
     })
     .catch(error => {
@@ -63,7 +47,6 @@ function download() {
         afterDownload();
     });
 }
-
 function afterDownload() {
     setTimeout(function() {
         var elementsToHide = document.getElementsByClassName("formElement");
@@ -73,7 +56,6 @@ function afterDownload() {
         document.getElementById('errorMessage').style.display='none';
         document.getElementById('successMessage').style.display='none';
         document.getElementById('url').value = ' ';
-
         location.reload();
     }, 4000);
 }
